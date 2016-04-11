3d704f012669d32530838f0965339add63ed3f3c
DariusAf <darius.afchar@live.fr>
2016-04-11 16:05:58 +0200

    Mise à jour de Neurone.h

--- C++/machine_learning/Neurone.h
+++ C++/machine_learning/Neurone.h
@@ -6,15 +6,16 @@
 
 #include <cstdlib>
 #include<ctime>
+using namespace std;
 
 class Perceptron {
 public:
     int n; //Taille de w
-    double* w;
     double theta; //Seuil
+    double* w;
     Perceptron();
     Perceptron(int n0);
-    set(double ww[], double ttheta); //Regler w a partir d'un ww
+    void set(double ww[], double ttheta); //Regler w a partir d'un ww
     int size() const;
     double seuil();
     double operator[](int i); //renvoie w[i]
@@ -29,8 +30,8 @@ class Echantillon {
     double** el; //Tableau des entrees
     double** v; //Tableau des sorties
 public:
-    int size() const;
     Echantillon(int p0, int n0, int k0);
+    int size() const;
     void set(int i, double ele[], double retour[]); //réglage de l'entree et de la sortie d'indice i
                                                     //a partir d'une entree et d'une sortie donnees
     void setEl(int i, int j, double d); //reglage de la case d'indice j de l'entree d'indice i de l'echantillon
@@ -45,7 +46,7 @@ public:
 class Reseau{
     int q;
     int nLmax;
-    double** z; //y est finalement inutile !
+    double** z; // tableau de sorties de perceptrons
     double** derE; //Tableau des dE/dyj
 public:
     Perceptron** p; //Tableau des perceptrons
@@ -55,6 +56,7 @@ public:
     double* retourSortie(); //renvoie la sortie stockee par le reseau
     void majP(double T[], double eps); //met a jour les perceptrons
     void apprend(Echantillon E, double eps); //algorithme d'apprentissage
+    void affiche();
 };
 
 
