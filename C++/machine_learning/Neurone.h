a5887015a6c39e022ba15ddd5b857f46591d6af9
DariusAf <darius.afchar@live.fr>
2016-04-11 17:51:01 +0200

    ÇA MARCHE !

--- C++/machine_learning/Neurone.h
+++ C++/machine_learning/Neurone.h
@@ -2,10 +2,6 @@
 
 #include <cmath>
 #include<iostream>
-#include <list>
-
-#include <cstdlib>
-#include<ctime>
 using namespace std;
 
 class Perceptron {
@@ -15,6 +11,7 @@ public:
     double* w;
     Perceptron();
     Perceptron(int n0);
+    ~Perceptron();
     void set(double ww[], double ttheta); //Regler w a partir d'un ww
     int size() const;
     double seuil();
@@ -31,26 +28,27 @@ class Echantillon {
     double** v; //Tableau des sorties
 public:
     Echantillon(int p0, int n0, int k0);
+    ~Echantillon();
     int size() const;
     void set(int i, double ele[], double retour[]); //réglage de l'entree et de la sortie d'indice i
                                                     //a partir d'une entree et d'une sortie donnees
     void setEl(int i, int j, double d); //reglage de la case d'indice j de l'entree d'indice i de l'echantillon
     void setT(int i, int j, double d); //reglage de la case d'indice j de la sortie d'indice i de l'echantillon
     double* getEl(int i); //renvoyer l'entree d'indice i de l'echantillon
-    double* tabT(int i); //renvoyer la sortie d'indice i de l'echantillon
+    double* tabT(int i); //renvoyer la sortie attendue d'indice i de l'echantillon
     double T(int i, int j); //renvoie l'element d'indice j de la sortie d'indice i de l'echantillon
-    ~Echantillon();
     double O(int i, Perceptron P); //calcule la reponse du perceptron a l'entree d'indice i de l'echantillon
 };
 
+
 class Reseau{
     int q;
-    int nLmax;
     double** z; // tableau de sorties de perceptrons
+    int* nLmax;
     double** derE; //Tableau des dE/dyj
 public:
     Perceptron** p; //Tableau des perceptrons
-    Reseau(int q0, int nL0);
+    Reseau(int q0, int nL[]);
     ~Reseau();
     void calculeZ(double I[]); //met a jour tous les z apres une entree I
     double* retourSortie(); //renvoie la sortie stockee par le reseau
